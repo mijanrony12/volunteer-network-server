5b981bcc5cd82d0b22cc807b23d39638570997ef
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-27 18:37:00 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zhcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create a function for data add
+async function run() {
+    try
+    {
+        await client.connect()
+        const database = client.db('network_volunteer');
+        const volunteerCollection= database.collection('volunteers')
+    }
+    finally
+    {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`listening the port:${port}`)
+})
